d9bd54f475b8fb4c5a58f784d34986b2d7b543ec
inkB12 <ttinhbao@gmail.com>
2025-11-03 19:56:26 +0700

    [FEAT] feat map

---
+++ EVCharging/Pages/Map/Index.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EVCharging.Pages.Map
+{
+    public class IndexModel : PageModel
+    {
+        public (double lat, double lng) Center { get; private set; }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
