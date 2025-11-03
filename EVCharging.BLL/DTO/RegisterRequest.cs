39912a3e5dd5a9af4638e6c985c8a9d253468820
inkB12 <ttinhbao@gmail.com>
2025-11-03 23:13:54 +0700

    [FEAT] Login + Register

--- EVCharging.BLL/DTO/RegisterRequest.cs
+++ EVCharging.BLL/DTO/RegisterRequest.cs
@@ -7,5 +7,6 @@ namespace EVCharging.BLL.DTO
         [Required, EmailAddress] public string Email { get; set; } = null!;
         [Required, MinLength(6)] public string Password { get; set; } = null!;
         [Required] public string FullName { get; set; } = null!;
+        [Required, Phone] public string Phone { get; set; } = null!;
     }
 }
