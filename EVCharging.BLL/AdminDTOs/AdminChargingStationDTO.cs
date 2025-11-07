9876586c3a21681b3a74055db4108e74f6720349
DESKTOP-R94SMKK\2111d <khanhtrantuc2004@gmail.com>
2025-11-07 22:07:13 +0700

    [UPDATE] Admin Service and DTO mapping Update

---
+++ EVCharging.BLL/AdminDTOs/AdminChargingStationDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVCharging.BLL.AdminDTOs
+{
+    public class AdminChargingStationDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Location { get; set; }
+        public string? Description { get; set; }
+        public string? Status { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longtitude { get; set; }
+
+        public List<AdminChargingPointDTO>? ChargingPoints { get; set; }
+    }
+}
---
+++ EVCharging.BLL/Interfaces/IAdminChargingStationService.cs
@@ -0,0 +1,19 @@
+﻿using EVCharging.BLL.AdminDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVCharging.BLL.Interfaces
+{
+    public interface IAdminChargingStationService
+    {
+        Task<List<AdminChargingStationDTO>> GetAllAsync();
+        Task<AdminChargingStationDTO?> GetByIdAsync(int id);
+        Task AddAsync(AdminChargingStationDTO dto);
+        Task UpdateAsync(AdminChargingStationDTO dto);
+        Task DeleteAsync(int id);
+        Task UpdateStatusAsync(int id, string status);
+    }
+}
