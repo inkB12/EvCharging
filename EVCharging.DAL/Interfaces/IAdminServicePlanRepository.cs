23632bcddbadaf8900b8fbc7bed4b29dc55575d3
DESKTOP-R94SMKK\2111d <khanhtrantuc2004@gmail.com>
2025-11-07 21:52:12 +0700

    [UPDATE] Admin Repo Update 01

---
+++ EVCharging.DAL/Interfaces/IAdminServicePlanRepository.cs
@@ -0,0 +1,18 @@
+﻿using EVCharging.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVCharging.DAL.Interfaces
+{
+    public interface IAdminServicePlanRepository
+    {
+        Task<List<ServicePlan>> GetAllAsync();
+        Task<ServicePlan?> GetByIdAsync(int id);
+        Task AddAsync(ServicePlan plan);
+        Task UpdateAsync(ServicePlan plan);
+        Task DeleteAsync(int id);
+    }
+}
